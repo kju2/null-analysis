aecd6619dde0fd16df969338dd53e443b9af14d6
kju2 <kju2@edlinger.me>
2014-12-08 20:13:21 +0100

    Add example for potential null pointer access.

--- src/main/java/com/github/kju2/nullanalysis/NullAnalysisExample.java
+++ src/main/java/com/github/kju2/nullanalysis/NullAnalysisExample.java
@@ -30,4 +30,26 @@ public class NullAnalysisExample {
 			// Do something!
 		}
 	}
+
+	/**
+	 * Limits of null analysis.
+	 * 
+	 * @see <a href="http://help.eclipse.org/luna/topic/org.eclipse.jdt.doc.user/tasks/task-using_null_annotations.htm">Copied from Eclipse documentation</a>
+	 */
+	public String flatten(String[] inputs1, String[] inputs2) {
+		StringBuffer sb1 = null, sb2 = null;
+		int len = Math.min(inputs1.length, inputs2.length);
+		for (int i = 0; i < len; i++) {
+			if (sb1 == null) {
+				sb1 = new StringBuffer();
+				sb2 = new StringBuffer();
+			}
+			sb1.append(inputs1[i]);
+			// WARNING: Potential null pointer access: The variable sb2 may be null at this location.
+			sb2.append(inputs2[i]);
+		}
+		if (sb1 != null)
+			return sb1.append(sb2).toString();
+		return "";
+	}
 }
